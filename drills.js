6ec141b2467e779ad82d3c7b18029452cd9796c3
Rose Sorfa <rose.otter.2015@gmail.com>
2018-10-11 15:09:06 -0500

    adding randomSort

--- drills.js
+++ drills.js
@@ -92,8 +92,8 @@ function bucketSort(list, lowest, highest) {
 //Sort in place
 //Write an algorithm to shuffle an array into a random order in-place 
 //(i.e. without creating a new array).
-function sortInPlace(list) {
-
+function sortInPlace(arr) {
+  
 }
 
 
