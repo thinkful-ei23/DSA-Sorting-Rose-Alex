37c7d07fde278c50655115cad2ab50350ea4eed3
Alex Puhl <apuhlswimmer@hotmail.com>
2018-10-11 12:28:36 -0600

    finished index.html

--- drills.js
+++ drills.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const arr = [89,30,25,32,72,70,51,42,25,24,53,55,78,50,13,40,48,32,26,2,14,33,45,72,56,44,21,88,27,68,15,62,93,98,73,28,16,46,87,28,65,38,67,16,85,63,23,69,64,91,9,70,81,27,97,82,6,88,3,7,46,13,11,64,76,31,26,38,28,13,17,69,90,1,6,7,64,43,9,73,80,98,46,27,22,87,49,83,6,39,42,51,54,84,34,53,78,40,14,5];
+const smallSet = [1,3,2,5,4]; // Should be 8 calls
 
 function qSort(arr, start=0, end=arr.length, count=0) {
   if(start >= end) { //base case
@@ -37,18 +38,19 @@ function swap(arr, i, j) {
   arr[j] = temp;
 }
 
-function mSort(arr) {
+function mSort(arr, count=0) {
+  count++;
   if(arr.length <= 1) { //base case
-    return arr;
+    return {arr, count};
   }
 
   const middle = Math.floor(arr.length / 2); 
   let left = arr.slice(0, middle);  //left = left half of the array
   let right = arr.slice(middle, arr.length); //right = right half of the array
 
-  left = mSort(left);
-  right = mSort(right);
-  return merge(left, right, arr);
+  left = mSort(left, count);
+  right = mSort(right, count);
+  return merge(left, right, arr, count);
 }
 
 function merge(left, right, arr) {
@@ -69,13 +71,14 @@ function merge(left, right, arr) {
   for(let i=rightIndex; i<right.length; i++) {
     arr[outputIndex++] = right[i];
   }
+  console.log(count);
   return arr;
 }
 
 
 function main() {
   // console.log(qSort(arr));
-  console.log(mSort(arr));
+  console.log(mSort(smallSet));
 }
 
 main();
